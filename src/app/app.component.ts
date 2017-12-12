4a04903a91984203f74db038e0bb0ff58794bf42
Podlipny <nick.47@seznam.cz>
2017-12-12 17:55:26 +0100

    Kongera initial wireframe

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import Parallax from 'parallax-js';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit  {
+  title = 'app';
+
+  ngOnInit() {
+    const scene = document.getElementById('scene');
+    const parallaxInstance = new Parallax(scene);
+  }
+
+}
